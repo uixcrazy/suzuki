28b9823bf7a4a3e18b8b8d2d01c606ce6ab3600b
uixcrazy <uixcrazy@gmail.com>
2019-06-17 14:59:47 +0700

    temp

--- components/index/Banner.js
+++ components/index/Banner.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Link from 'next/link';
 
-
 const useStyles = makeStyles(theme => ({
   banner: {
     height: '100%',
@@ -13,7 +12,12 @@ const useStyles = makeStyles(theme => ({
     position: 'relative',
     '& img': {
       display: 'block',
-      width: '100%',
+      // width: '100%',
+      width: '100vw',
+      objectFit: 'cover',
+      objectPosition: 'center',
+      // max-width: 100%;
+      // height: 'auto',
     }
   }
 }));
@@ -23,7 +27,7 @@ const Banner = props => {
   return (
     <div className={classes.banner}>
       {props.isMobile
-      ? <Link href="/">
+      ? <Link href="www.google.com">
         <a className={classes.bannerLink}>
           <img
             src="https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
@@ -31,7 +35,7 @@ const Banner = props => {
           />
         </a>
       </Link>
-    :<Link href="/">
+    : <Link href="wwww.google.com">
       <a className={classes.bannerLink}>
         <img
           src="https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
